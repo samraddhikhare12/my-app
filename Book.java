ca41e31665168ac354c6c27ca2d0b3273a9dd5c2
Samraddhi Khare <samriddhi.khare42@gmail.com>
2019-09-30 12:28:11 -0700

    modified book.java

--- Book.java
+++ Book.java
@@ -2,20 +2,36 @@ class Book{
        
 	   private int bookId;
 	   private String name;
+	   private String author;
        
 	   
 	   public int setbookId(int bookId)
 	   {
 	   this.bookId = bookId;
+	   System.out.println("bookId is set");
 	   }
 	   public void get.bookId()
 	   {
 		   return bookId;
 	   }
 	    
+
 	   public String setname(String name)
 	   {
 		   this.name=name;
 		   System.out.println("this is my book");
 	   }
-	   
+	    public void getName()
+		{
+			return name;
+		}
+		private String setAuthor(String author)
+		{
+			this.author=author;
+			System.out.println("author is here");
+			
+		}
+		public void getAuthor()
+		{
+			return author;
+		}
